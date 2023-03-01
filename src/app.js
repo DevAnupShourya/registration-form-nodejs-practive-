c309f7de53c47caff9068bbe728e8b79c3fd17e8
DevAnupShourya <contact.yourwebdev@gmail.com>
2023-03-01 18:22:50 +0530

    hashing enabled

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const hbs = require('hbs');
+const bcryptjs = require('bcryptjs');
 const port = process.env.PORT || 8080;
 
 app.use(express.json())
@@ -9,49 +10,59 @@ app.use(express.urlencoded({ extended: false }))
 
 // ? DB imports
 require('./db/connection');
+// ? Schema Importing
 const Register = require('./models/register');
-
 // ? paths of all important dirs
 const static_path = path.join(__dirname, "../public/");
 const views_path = path.join(__dirname, "../templates/views");
 const partials_path = path.join(__dirname, "../templates/partials");
-
+// ? Redeclaring some predefined stffs
 app.use(express.static(static_path));
 app.set('view engine', "hbs");
 app.set('views', views_path)
-
+// ? Using partials
 hbs.registerPartials(partials_path);
-
+// ! Home Page Loading
 app.get('/', (req, res) => {
     // res.send('<h1>This is is home page</h1>')
     res.render('index');
 });
+// ! Login Page Loading
 app.get('/login', (req, res) => {
     // res.send('<h1>This is is login page</h1>')
     res.render('login');
 });
+// ! Login Hapenning
 app.post('/login', async (req, res) => {
     try {
         const email = req.body.email;
         const password = req.body.password;
-        
-        // ? checking that email is available in datanase or not
-        const userEmail = await Register.findOne({email});
-        if(userEmail.password === password){
-            res.status(201).render('index');
-        }else{
-            res.send("Invalid Details!!")
-        }
+        // * console.log(`email: ${email} , pass : ${password}`);
+        if (email === '' || password === '') {
+            res.send('Fill all inputs!!!')
+        } else {
+            // ? checking that email is available in datanase or not
+            const userEmail = await Register.findOne({ email });
+            const passward_is_matching = await bcryptjs.compare(password, userEmail.password);
 
+            if (passward_is_matching) {
+                res.status(201).render('index');
+                console.log(`${userEmail.full_name} logged in...`);
+            } else {
+                res.send("Invalid Details!!")
+            }
+        }
     } catch (error) {
-        res.status(400).send(`Error : ${error}`)
+        res.status(400).send(`Something went wrong: ${error}`)
     }
-    
+
 });
+// ! Registration Page Loading
 app.get('/reg', (req, res) => {
     // res.send('<h1>This is is registrations page</h1>')
     res.render('reg');
 });
+// ! Registration Hapenning
 // ? getting data from user
 app.post('/reg', async (req, res) => {
     try {
@@ -77,7 +88,7 @@ app.post('/reg', async (req, res) => {
         res.status(400).send(error);
     }
 });
-
+// ! Raising the port
 app.listen(port, () => {
     console.log(`server running at http://localhost:${port}/`);
 })
--- src/models/register.js
+++ src/models/register.js
@@ -1,33 +1,44 @@
 const mongoose = require('mongoose');
+const bcryptjs = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-    full_name :{
+    full_name: {
         type: String,
-        required:true
+        required: true
     },
-    email :{
+    email: {
         type: String,
         required: true,
         unique: true
     },
-    phone :{
+    phone: {
         type: Number,
         required: true,
         unique: true
     },
-    age :{
+    age: {
         type: Number,
         required: true,
     },
-    password :{
+    password: {
         type: String,
-        required:true
+        required: true
     },
-    confirm_password :{
+    confirm_password: {
         type: String,
-        required:true
+        required: true
     }
 });
+
+// ? hashing password (middleware)
+userSchema.pre("save", async function (next) {
+    if(this.isModified){
+        this.password = await bcryptjs.hash(this.password, 10);
+        this.confirm_password = undefined;
+    }
+    next();
+})
+
 // ? creating a collection
-const Register = new mongoose.model('Register',userSchema);
+const Register = new mongoose.model('Register', userSchema);
 module.exports = Register;
